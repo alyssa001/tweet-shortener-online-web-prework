218e3b96a75c6a1e71448bf17e9ea6a522df3d7e
Alyssa Yesilyurt <alyssa.yesilyurt@gmail.com>
2019-06-03 15:49:35 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -40,7 +40,7 @@ end
 
 def shortened_tweet_truncator(tweet)
   if tweet.length>140
-    return_tweet = tweet[0..140] + "..."
+    return_tweet = tweet[0..136] + "..."
   else 
     return_tweet = tweet
   end
