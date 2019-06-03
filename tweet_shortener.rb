d17c9d0b7e438af1423a0dc54df7dbef99ac7262
Alyssa Yesilyurt <alyssa.yesilyurt@gmail.com>
2019-06-03 15:45:14 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -29,6 +29,13 @@ def bulk_tweet_shortener(tweet)
   end 
 end 
 
-def 
-  
+def selective_tweet_shortener(tweet) 
+  tweet.split(" ").map do |phrase|
+    if phrase.length > 140
+      word_substituter(phrase)
+    elsif phrase.length <= 140
+      phrase
+    end 
+  end
 end 
+
